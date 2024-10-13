143d9b36cce834286e34032f2bb0e3df905504d2
Cesar Anchiraico <22200561@ue.edu.pe>
2024-10-12 19:53:08 -0500

    Agregar archivos de proyecto.

---
+++ Parcial20240222200561DB/Infraestructure/Data/Mechanic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parcial20240222200561DB.Infraestructure.Data;
+
+public partial class Mechanic
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Email { get; set; }
+
+    public DateOnly HireDate { get; set; }
+}
---
+++ Parcial20240222200561DB/Infraestructure/Data/Parcial20240222200561DbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parcial20240222200561DB.Infraestructure.Data;
+
+public partial class Parcial20240222200561DbContext : DbContext
+{
+    public Parcial20240222200561DbContext()
+    {
+    }
+
+    public Parcial20240222200561DbContext(DbContextOptions<Parcial20240222200561DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Mechanic> Mechanic { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=Parcial20240222200561DB;Integrated Security=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Mechanic>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Mechanic__3214EC070E24670A");
+
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.PhoneNumber).HasMaxLength(15);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
